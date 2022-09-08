87b78ee2b064c7425432c885b32be05f998c6fa4
nijiferanmi <feranmi@kickgame.co.uk>
2022-09-08 02:44:10 +0100

    progress

--- src/components/landing/DrivingUs.js
+++ src/components/landing/DrivingUs.js
@@ -1,8 +1,49 @@
 import React from "react";
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 
 const DrivingUs = () => {
-  return <div className="driving-us">
-    
-  </div>;
+  return (
+    <>
+    <div className="driving-us">
+      <img
+        src={require("../../assets/driving-us.svg")}
+        alt="what is driving us"
+      />
+      <div className="driving-text">
+        <h5>What’s Driving Us?</h5>
+        <h6>Sensitizing Africa towards modern agriculture. Transforming Agric of culture to business through agribusiness consultancy, financing, branding, marketing,</h6>
+        <div className="text-author">
+          <button>Watch our Story <PlayCircleIcon /> </button>
+          <div>
+            <h4>Umar Keefa</h4>
+            <h6>CEO, Agcademy</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="driving-m">
+      <div className="m-d">
+      <img
+        src={require("../../assets/driving-us.svg")}
+        alt="what is driving us"
+      />
+      <div className="driving-text">
+        <div className="text-author">
+          <button>Watch our Story <PlayCircleIcon /> </button>
+          <div>
+            <h4>Umar Keefa</h4>
+            <h6>CEO, Agcademy</h6>
+          </div>
+        </div>
+      </div>
+      </div>
+      <div className="driving-text m">
+        <h5>What’s Driving Us?</h5>
+        <h6>Sensitizing Africa towards modern agriculture. Transforming Agric of culture to business through agribusiness consultancy, financing, branding, marketing.</h6>
+      </div>
+    </div>
+
+    </>
+  );
 };
 export default DrivingUs;
