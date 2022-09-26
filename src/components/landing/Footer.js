a76cb185e5a04ef0553339c6c0f132491bee9d17
nijiferanmi <feranmi@kickgame.co.uk>
2022-09-26 07:57:04 +0100

    Footer

--- src/components/landing/Footer.js
+++ src/components/landing/Footer.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 
 const Footer = () => {
@@ -9,14 +10,44 @@ const Footer = () => {
                         <h5>agcademy</h5>
                         <img src={require('../../assets/Sitting.svg')} alt="Footer"/>
                     </div>
-
+                    <div>
+                        <h6>Products</h6>
+                        <a>Courses</a>
+                        <a>Live Events</a>
+                        <a>Resource Kits</a>
+                        <a>Instructor Guides</a>
+                    </div>
+                    <div>
+                        <h6>Company</h6>
+                        <a>About Us</a>
+                        <a>Our Culture</a>
+                        <a>FAQs</a>
+                        <a>Blog</a>
+                    </div>
                 </div>
                 <div className='foot-links-two'>
-
+                    <h6>&copy; agcademy.com 2022</h6>
+                    <a>Privacy Policy</a>
+                    <a>Terms of Use</a>
                 </div>
             </div>
             <div className='footer-desc'>
-
+                <img src={require('../../assets/logo-white.svg')} alt="Logo agcademy"/>
+                <p>To increase the growth of agric farmers and food landscape in Africa, a decentralised online education to help farmers.</p>
+                <div className='social-links'>
+                    <a className='twitter'>
+                        <img src={require('../../assets/twitter.svg')} alt="Twitter social"/>
+                    </a>
+                    <a className='linkedin'>
+                        <img src={require('../../assets/linkedin.svg')} alt="linkedin social"/>
+                    </a>
+                    <a className='facebook'>
+                        <img src={require('../../assets/facebook.svg')} alt="facebook social"/>
+                    </a>
+                    <a className='instagram'>
+                        <img src={require('../../assets/instagram.svg')} alt="instagram social"/>
+                    </a>
+                </div>
             </div>
         </div>
     )
